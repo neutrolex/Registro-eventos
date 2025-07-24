35f0dda805c3e70d02444ab2488e986ef420e3c8
ZaithFSG <zaithfabrizio@gmail.com>
2025-07-24 16:54:32 -0500

    confirm

--- registro/tests.py
+++ registro/tests.py
@@ -1,5 +1,6 @@
+from django.test import TestCase
 from django.urls import reverse
-from .models import Event
+from .models import Evento
 
 class TestEventoDeleteView(TestCase):
     def setUp(self):
